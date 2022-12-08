66b728d10748b593694fbf0ad3f16bf8fc768a7c
Brian Maher <bmaher91@gmail.com>
2022-12-07 19:11:32 -0700

    day 6 part 2

--- day-6/day-6.js
+++ day-6/day-6.js
@@ -2,28 +2,35 @@ import { readFileSync } from 'fs';
 
 const dataStream = readFileSync('day-6/day-6-input.txt', 'utf-8').split('');
 
-const queue = [];
-let i = 0;
-for (; i < dataStream.length; i++) {
-  const char = dataStream[i];
-  if (queue.length < 4) {
-    queue.push(char);
-  } else {
-    let anyMatch = false;
-    for (let j = 0; j < 4; j++) {
-      for (let k = 0; k < 4; k++) {
-        if (j !== k && queue[j] === queue[k]) {
-          anyMatch = true;
-        }
-      }
-    }
-    if (anyMatch) {
-      queue.shift();
+const findStartOfStreamMarker = (numDistinct) => {
+  const queue = [];
+  let i = 0;
+
+  for (; i < dataStream.length; i++) {
+    const char = dataStream[i];
+    if (queue.length < numDistinct) {
       queue.push(char);
     } else {
-      break;
+      let anyMatch = false;
+      for (let j = 0; j < numDistinct; j++) {
+        for (let k = 0; k < numDistinct; k++) {
+          if (j !== k && queue[j] === queue[k]) {
+            anyMatch = true;
+          }
+        }
+      }
+      if (anyMatch) {
+        queue.shift();
+        queue.push(char);
+      } else {
+        break;
+      }
     }
   }
+
+  return i;
 }
 
-console.log('part 1: ', i);
+
+console.log('part 1: ', findStartOfStreamMarker(4));
+console.log('part 2: ', findStartOfStreamMarker(14));
