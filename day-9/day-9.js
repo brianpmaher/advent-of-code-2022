704768b72aac9495a095d7ab3bb97d919e3fb0b6
Brian Maher <bmaher91@gmail.com>
2022-12-11 09:28:18 -0700

    day 9 part 2

--- day-9/day-9.js
+++ day-9/day-9.js
@@ -5,16 +5,20 @@ const DOWN = 'D';
 const LEFT = 'L';
 const RIGHT = 'R';
 
-const instructions = readFileSync('day-9/day-9-input.txt', 'utf-8')
+const fileLines = readFileSync('day-9/day-9-input.txt', 'utf-8')
     .replace(/\r/g, '')
-    .split('\n')
-    .map(line => {
-        const [direction, numStr] = line.split(' ');
-        return {
-            direction,
-            number: parseInt(numStr),
-        };
-    });
+    .split('\n');
+
+function parseInstructions(rawList) {
+    return rawList
+        .map(line => {
+            const [direction, numStr] = line.split(' ');
+            return {
+                direction,
+                number: parseInt(numStr),
+            };
+        });
+}
 
 const pos = (x, y) => ({ x, y });
 const clone = ({ x, y }) => pos(x, y);
@@ -64,71 +68,79 @@ const clone = ({ x, y }) => pos(x, y);
 // }
 
 // Part 2
-let segments = [];
-for (let i = 0; i < 9; i++) {
-    segments.push(pos(0, 0));
-}
+function ropeSim(instructions) {
+    const numSegments = 10;
+    let segments = [];
+    for (let i = 0; i < numSegments; i++) {
+        segments.push(pos(0, 0));
+    }
 
-const head = segments[0];
-const tail = segments[8];
+    const head = segments[0];
+    const tail = segments[numSegments - 1];
 
-let visitedPositions = [clone(tail)];
+    let visitedPositions = [clone(tail)];
 
-for (const instruction of instructions) {
-    // for each movement
-    for (let i = 0; i < instruction.number; i++) {
-        // Move head
-        switch (instruction.direction) {
-            case UP:
-                head.y++;
-                break;
-            case DOWN:
-                head.y--;
-                break;
-            case LEFT:
-                head.x--;
-                break;
-            case RIGHT:
-                head.x++;
-                break;
-        }
-
-        // for each rope segment
-        for (let j = 0; j < 8; j++) {
-            const newHead = segments[j];
-            const newTail = segments[j+1]
-
-            // Check if tail is too far
-            const delta = pos(newHead.x - newTail.x, newHead.y - newTail.y);
-            if (delta.x === 2 && delta.y === 0) {
-                newTail.x++;
-            } else if (delta.x === -2 && delta.y === 0) {
-                newTail.x--;
-            } else if (delta.x === 0 && delta.y === 2) {
-                newTail.y++;
-            } else if (delta.x === 0 && delta.y === -2) {
-                newTail.y--;
-            } else if ((delta.x === 1 && delta.y === 2) || (delta.x === 2 && delta.y === 1) || (delta.x === 2 && delta.y === 2)) {
-                newTail.x++;
-                newTail.y++;
-            } else if ((delta.x === -1 && delta.y === 2) || (delta.x === -2 && delta.y === 1) || (delta.x === -2 && delta.y === 2)) {
-                newTail.x--;
-                newTail.y++;
-            } else if ((delta.x === 1 && delta.y === -2) || (delta.x === 2 && delta.y === -1) || (delta.x === 2 && delta.y === -2)) {
-                newTail.x++;
-                newTail.y--;
-            } else if ((delta.x === -1 && delta.y === -2) || (delta.x === -2 && delta.y === -1) || (delta.x === -2 && delta.y === -2)) {
-                newTail.x--;
-                newTail.y--;
+    for (const instruction of instructions) {
+        // for each movement
+        for (let i = 0; i < instruction.number; i++) {
+            // Move head
+            switch (instruction.direction) {
+                case UP:
+                    head.y++;
+                    break;
+                case DOWN:
+                    head.y--;
+                    break;
+                case LEFT:
+                    head.x--;
+                    break;
+                case RIGHT:
+                    head.x++;
+                    break;
             }
-        }
 
-        if (!visitedPositions.some(visitedPosition => visitedPosition.x === tail.x && visitedPosition.y === tail.y)) {
-            visitedPositions.push(clone(tail));
+            // for each rope segment
+            for (let j = 0; j < numSegments - 1; j++) {
+                const newHead = segments[j];
+                const newTail = segments[j + 1]
+
+                // Check if tail is too far
+                const delta = pos(newHead.x - newTail.x, newHead.y - newTail.y);
+                if (delta.x === 2 && delta.y === 0) {
+                    newTail.x++;
+                } else if (delta.x === -2 && delta.y === 0) {
+                    newTail.x--;
+                } else if (delta.x === 0 && delta.y === 2) {
+                    newTail.y++;
+                } else if (delta.x === 0 && delta.y === -2) {
+                    newTail.y--;
+                } else if ((delta.x === 1 && delta.y === 2) || (delta.x === 2 && delta.y === 1) || (delta.x === 2 && delta.y === 2)) {
+                    newTail.x++;
+                    newTail.y++;
+                } else if ((delta.x === -1 && delta.y === 2) || (delta.x === -2 && delta.y === 1) || (delta.x === -2 && delta.y === 2)) {
+                    newTail.x--;
+                    newTail.y++;
+                } else if ((delta.x === 1 && delta.y === -2) || (delta.x === 2 && delta.y === -1) || (delta.x === 2 && delta.y === -2)) {
+                    newTail.x++;
+                    newTail.y--;
+                } else if ((delta.x === -1 && delta.y === -2) || (delta.x === -2 && delta.y === -1) || (delta.x === -2 && delta.y === -2)) {
+                    newTail.x--;
+                    newTail.y--;
+                }
+            }
+
+            const hasNotVisitedAlready = !visitedPositions.some(visitedPosition => visitedPosition.x === tail.x && visitedPosition.y === tail.y);
+            if (hasNotVisitedAlready)
+                visitedPositions.push(clone(tail));
         }
     }
+
+    return visitedPositions.length;
 }
 
 // TODO: 1 is too low
+// TODO: 81 is wrong
 // TODO: 2642 is too high
-console.log('num visited', visitedPositions.length);
+const largeExample = ['R 5', 'U 8', 'L 8', 'D 3', 'R 17', 'D 10', 'L 25', 'U 20'];
+const smallExample = ['R 4', 'U 4', 'L 3', 'D 1', 'R 4', 'D 1', 'L 5', 'R 2'];
+console.log('num visited', ropeSim(parseInstructions(fileLines)));
